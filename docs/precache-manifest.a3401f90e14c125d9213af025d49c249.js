487e4c2443ffbdd4a555be18186a0edc540f8f83
Will Willis <will.willis@gmail.com>
2019-12-31 00:29:11 -0600

    adding docs for github pages

---
+++ docs/precache-manifest.a3401f90e14c125d9213af025d49c249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54875c93e10f1a06de68",
+    "url": "/static/css/main.655b1ab1.chunk.css"
+  },
+  {
+    "revision": "54875c93e10f1a06de68",
+    "url": "/static/js/main.54875c93.chunk.js"
+  },
+  {
+    "revision": "93ea7b44a3006f8fa25d",
+    "url": "/static/css/1.68d231d9.chunk.css"
+  },
+  {
+    "revision": "93ea7b44a3006f8fa25d",
+    "url": "/static/js/1.93ea7b44.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c56e04d299be1baba0af63a93b4cfad",
+    "url": "/index.html"
+  }
+];
