d6176e523dbb264934c37d20ef52f4304c0be91a
Michael Ferber <mferber8@gmail.com>
2020-01-11 13:59:47 -0600

    lassie sync codegen

---
+++ src/graphql/mutations.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createSensor = `mutation CreateSensor($input: CreateSensorInput!) {
+  createSensor(input: $input) {
+    id
+    temp
+    smoke
+    lat
+    long
+    timestamp
+  }
+}
+`;
+export const updateSensor = `mutation UpdateSensor($input: UpdateSensorInput!) {
+  updateSensor(input: $input) {
+    id
+    temp
+    smoke
+    lat
+    long
+    timestamp
+  }
+}
+`;
+export const deleteSensor = `mutation DeleteSensor($input: DeleteSensorInput!) {
+  deleteSensor(input: $input) {
+    id
+    temp
+    smoke
+    lat
+    long
+    timestamp
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onUpdateSensor = `subscription OnUpdateSensor($id: ID!) {
+  onUpdateSensor(id: $id) {
+    id
+    temp
+    smoke
+    lat
+    long
+    timestamp
+  }
+}
+`;
