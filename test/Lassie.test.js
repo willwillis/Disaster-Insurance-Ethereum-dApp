874c40ee37bdd53e85ae399e4bb44b2a5110bdd4
Will Willis <will.willis@gmail.com>
2020-01-25 08:17:24 -0600

    tests

--- test/Lassie.test.js
+++ test/Lassie.test.js
@@ -1,103 +1,68 @@
-const Marketplace = artifacts.require('./Marketplace.sol')
+const Lassie = artifacts.require("./Lassie.sol");
 
-require('chai')
-  .use(require('chai-as-promised'))
-  .should()
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
 
-contract('Marketplace', ([deployer, seller, buyer]) => {
-  let marketplace
+contract("Lassie", ([deployer, manager]) => {
+  let lassie;
 
   before(async () => {
-    marketplace = await Marketplace.deployed()
-  })
+    lassie = await Lassie.deployed();
+  });
 
-  describe('deployment', async () => {
-    it('deploys successfully', async () => {
-      const address = await marketplace.address
-      assert.notEqual(address, 0x0)
-      assert.notEqual(address, '')
-      assert.notEqual(address, null)
-      assert.notEqual(address, undefined)
-    })
+  describe("deployment", async () => {
+    it("deploys successfully", async () => {
+      const address = await lassie.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
 
-    it('has a name', async () => {
-      const name = await marketplace.name()
-      assert.equal(name, 'Dapp University Marketplace')
-    })
-  })
+    it("has a name", async () => {
+      const name = await lassie.name();
+      assert.equal(name, "lassie");
+    });
+  });
 
-  describe('products', async () => {
-    let result, productCount
+  describe("sensors", async () => {
+    let result, sensorCount;
 
     before(async () => {
-      result = await marketplace.createProduct('iPhone X', web3.utils.toWei('1', 'Ether'), { from: seller })
-      productCount = await marketplace.productCount()
-    })
+      result = await lassie.createSensor(
+        "Sydney",
+        web3.utils.toWei("1", "Ether"),
+        { from: manager }
+      );
+      sensorCount = await lassie.sensorCount();
+    });
 
-    it('creates products', async () => {
+    it("creates sensors", async () => {
       // SUCCESS
-      assert.equal(productCount, 1)
-      const event = result.logs[0].args
-      assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
-      assert.equal(event.name, 'iPhone X', 'name is correct')
-      assert.equal(event.price, '1000000000000000000', 'price is correct')
-      assert.equal(event.owner, seller, 'owner is correct')
-      assert.equal(event.purchased, false, 'purchased is correct')
+      assert.equal(sensorCount, 1);
+      const event = result.logs[0].args;
+      assert.equal(
+        event.id.toNumber(),
+        sensorCount.toNumber(),
+        "id is correct"
+      );
+      assert.equal(event.name, "Sydney", "name is correct");
 
-      // FAILURE: Product must have a name
-      await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
-      // FAILURE: Product must have a price
-      await await marketplace.createProduct('iPhone X', 0, { from: seller }).should.be.rejected;
-    })
+      // FAILURE: Sensor must have a name
+      await await lassie.createSensor("", web3.utils.toWei("1", "Ether"), {
+        from: manager
+      }).should.be.rejected;
+    });
 
-    it('lists products', async () => {
-      const product = await marketplace.products(productCount)
-      assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
-      assert.equal(product.name, 'iPhone X', 'name is correct')
-      assert.equal(product.price, '1000000000000000000', 'price is correct')
-      assert.equal(product.owner, seller, 'owner is correct')
-      assert.equal(product.purchased, false, 'purchased is correct')
-    })
-
-    it('sells products', async () => {
-      // Track the seller balance before purchase
-      let oldSellerBalance
-      oldSellerBalance = await web3.eth.getBalance(seller)
-      oldSellerBalance = new web3.utils.BN(oldSellerBalance)
-
-      // SUCCESS: Buyer makes purchase
-      result = await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether')})
-
-      // Check logs
-      const event = result.logs[0].args
-      assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
-      assert.equal(event.name, 'iPhone X', 'name is correct')
-      assert.equal(event.price, '1000000000000000000', 'price is correct')
-      assert.equal(event.owner, buyer, 'owner is correct')
-      assert.equal(event.purchased, true, 'purchased is correct')
-
-      // Check that seller received funds
-      let newSellerBalance
-      newSellerBalance = await web3.eth.getBalance(seller)
-      newSellerBalance = new web3.utils.BN(newSellerBalance)
-
-      let price
-      price = web3.utils.toWei('1', 'Ether')
-      price = new web3.utils.BN(price)
-
-      const exepectedBalance = oldSellerBalance.add(price)
-
-      assert.equal(newSellerBalance.toString(), exepectedBalance.toString())
-
-      // FAILURE: Tries to buy a product that does not exist, i.e., product must have valid id
-      await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;      // FAILURE: Buyer tries to buy without enough ether
-      // FAILURE: Buyer tries to buy without enough ether
-      await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether') }).should.be.rejected;
-      // FAILURE: Deployer tries to buy the product, i.e., product can't be purchased twice
-      await marketplace.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
-      // FAILURE: Buyer tries to buy again, i.e., buyer can't be the seller
-      await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
-    })
-
-  })
-})
+    it("lists sensors", async () => {
+      const sensor = await lassie.sensors(sensorCount);
+      assert.equal(
+        sensor.id.toNumber(),
+        sensorCount.toNumber(),
+        "id is correct"
+      );
+      assert.equal(sensor.name, "Sydney", "name is correct");
+    });
+  });
+});
